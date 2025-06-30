9ba4c30f52b5da30f78b80aeff8ee108e81b10c8
gsincodes <gsincodes@gmail.com>
2025-06-30 12:42:47 +0530

    Add basic UI components in place

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./styles.css";
 
-console.log("sup");
+class Todo{
+    constructor(title, description, dueDate = null, priority = null, notes = null){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    toggleCompleted(){
+        this.completed = !this.completed;
+    }
+}
+
+const newTodo = new Todo("todo#1", "This is a new todo.");
+newTodo.toggleCompleted();
+
+console.log(newTodo);
+
+class defaultProject{
+    constructor(todo){
+        this.todo = todo;
+    }
+}
+
+const DP = new defaultProject(newTodo);
+console.log(DP);
